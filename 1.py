ffaa1c72a9970d67aa93e2f8c6146226b55882e1
suliang <501218549@qq.com>
2018-08-20 11:03:06 +0800

    first commit

---
+++ 1.py
@@ -0,0 +1,7 @@
+import cal
+c = cal.Calculator("alex",18,'male')
+
+c.speakname()
+c.speakage()
+c.speaksex()
+cal.hello()
---
+++ cal.py
@@ -0,0 +1,17 @@
+class Calculator:
+    def __init__(self,name,age,sex):
+        self.name = name
+        self.age = age
+        self.sex = sex
+
+    def speakname(self):
+        print(self.name)
+
+    def speakage(self):
+        print(self.age)
+
+    def speaksex(self):
+        print(self.sex)
+
+def hello():
+    print("hello world")
